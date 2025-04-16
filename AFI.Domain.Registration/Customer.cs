9b564d1571e3e8a363aa640c0c3007c75707667b
Paul <phelpspuk@outlook.com>
2025-04-16 15:05:40 +0100

    Creating Customer domain object and adding validation rules along with unit tests.

---
+++ AFI.Domain.Registration/Customer.cs
@@ -0,0 +1,99 @@
+﻿using AFI.Domain.Registration.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AFI.Domain.Registration
+{
+    public class Customer : ICustomer
+    {
+        public static Customer CreateNew(
+            string firstName,
+            string surname,
+            string policyReferenceNumber,
+            DateTime? dateOfBirth,
+            string? email)
+        {
+            return new Customer(
+                firstName,
+                surname,
+                policyReferenceNumber,
+                dateOfBirth,
+                email);
+        }
+
+        private Customer(
+            string firstName,
+            string surname,
+            string policyReferenceNumber,
+            DateTime? dateOfBirth,
+            string? email)
+        {
+            if (string.IsNullOrWhiteSpace(firstName) || firstName.Length < 3 || firstName.Length > 50)
+            {
+                throw new ValidationException("First name must be between 3 and 50 characters long.");
+            }
+
+            if (string.IsNullOrWhiteSpace(surname) || surname.Length < 3 || surname.Length > 50)
+            {
+                throw new ValidationException("Surname must be between 3 and 50 characters long.");
+            }
+
+            if (!Regex.IsMatch(policyReferenceNumber, RegexPatterns.PolicyReferencePattern))
+            {
+                throw new ValidationException(
+                    $"Policy Reference Number: {policyReferenceNumber} does not conform to the expected format.");
+            }
+
+            if (dateOfBirth == null && string.IsNullOrWhiteSpace(email))
+            {
+                throw new ValidationException("Date of Birth or Email required.");
+            }
+
+            DateTime eighteenYearsFromToday = DateTime.Today.AddYears(-18);
+            if (dateOfBirth != null && !IsOver18(dateOfBirth))
+            {
+                throw new ValidationException("Policy holder must be 18 or older.");
+            }
+
+            if (!string.IsNullOrWhiteSpace(email) && !Regex.IsMatch(email, RegexPatterns.EmailAddressPattern))
+            {
+                throw new ValidationException("Email does not match the expected email format.");
+            }
+
+            FirstName = firstName;
+            Surname = surname;
+            PolicyReferenceNumber = policyReferenceNumber;
+            DateOfBirth = dateOfBirth;
+            Email = email;
+        }
+
+        public int CustomerId { get; private set; }
+
+        public string FirstName { get; }
+        
+        public string Surname { get; }
+        
+        public string PolicyReferenceNumber { get; }
+        
+        public DateTime? DateOfBirth { get; }
+        
+        public string? Email { get; }
+
+        public void Register(ICustomerRegistrationRepository repository)
+        {
+            CustomerId = 1;
+        }
+
+        private bool IsOver18(DateTime? dateToCheck)
+        {
+            DateTime eighteenYearsAgoToday = DateTime.Today.AddYears(-18);
+
+            return dateToCheck <= eighteenYearsAgoToday;
+        }
+    }
+}
